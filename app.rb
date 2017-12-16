cfd0d5defde66ca2dfc52142e033a972b3a6bfb8
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-16 20:42:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,4 +19,10 @@ class App < Sinatra::Base
     "Goodbye #{@user_name}!"
   end
 
+  get '/multiply/:number1/:number2' do
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    "#{@number1 * @number2}"
+  end
+
 end
